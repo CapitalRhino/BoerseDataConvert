d9358ed1bfc099e2417cca4d3765bd4c551e89cf
lastvoidtemplar <jed205@abv.bg>
2021-07-07 11:41:32 +0300

    Update Program.cs

--- BoerseDataConvert/BoerseDataConvert/Views/Program.cs
+++ BoerseDataConvert/BoerseDataConvert/Views/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.IO.Compression;
+using System.Linq;
 
 namespace BoerseDataConvert
 {
@@ -12,7 +15,18 @@ namespace BoerseDataConvert
             // -d directory or --dir directory
             // -o directory or --output direcory
             // -h - help
-
+            string s = "900#01|204#J|008#HSH Nordbank AG|205#150215|206#111138|460#HSH Nordbank|207#HSH Nordbank AG#Gerhart-Hauptmann-Platz 50#20095 Hamburg#Deutschland|208#info@hsh-nordbank.com|209#040 33330|210#https://www.hsh-nordbank.de|451#TUKDD90GPC79G1KOE162";
+            string[] sr = s.Split("|").ToArray();
+            Dictionary<string, string> a = new Dictionary<string, string>();
+            foreach (var item in sr)
+            {
+                string[] d = item.Split('#').ToArray();
+                a.Add(d[0], d[1]);
+            }
+            Record record = new Record();
+            record.TagsValues = a;
+            RecordController con = new RecordController("oo");
+            Console.WriteLine(con.ConvertToXml(record));
             Console.WriteLine("Hello World!");
         }
     }
