36978e9fe4d0a993d580c4621914048a2612d7f8
lastvoidtemplar <jed205@abv.bg>
2021-07-07 17:51:55 +0300

    Create Writer.cs

---
+++ BoerseDataConvert/BoerseDataConvert/Views/Writer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoerseDataConvert
+{
+    public class Writer
+    {
+        private static string curFilesName;
+        private static StreamWriter writer;
+
+        public Writer(string filesName)
+        {
+            curFilesName = filesName;
+            string[] file = curFilesName.Split('.').ToArray();
+            writer = new StreamWriter($"{file[0]}.xml");
+            writer.WriteLine($"<table name=”{file[0]}”>");
+        }
+        public void WriteRecord(string record)
+        {
+            writer.WriteLine(record);
+        }
+        public static void NextFile(string fileName)
+        {
+            EndFile();
+            curFilesName = fileName;
+            string[] file = curFilesName.Split('.').ToArray();
+            writer = new StreamWriter($"{file[0]}.xml");
+            writer.WriteLine($"<table name=”{file[0]}”>");
+        }
+        public static void EndFile()
+        {
+            writer.WriteLine("</table>");
+            writer.Close();
+        }
+    }
+}
